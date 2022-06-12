4c02e8b13d5ba498f15b9fa1e26a98f280434235
CarolinaYoko <yokoyamacarolina@gmail.com>
2022-06-12 11:05:14 -0300

    first commit

---
+++ TP_4/AppClinica/FrmCarga.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppClinica
+{
+    public partial class FrmRecordatorio : Form
+    {
+        public FrmRecordatorio()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmRecordatorio_Load(object sender, EventArgs e)
+        {
+            pbRecordatorio.Value = 0;
+        }
+
+        private void FrmRecordatorio_Activated(object sender, EventArgs e)
+        {
+            while (pbRecordatorio.Value < 100) {
+
+                pbRecordatorio.Value += 10;
+            }
+        }
+    }
+}
