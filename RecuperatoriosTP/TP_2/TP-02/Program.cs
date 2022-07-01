1bafbbe252a5c1d227e3da9a8344ec1bf33c7aa4
CarolinaYoko <yokoyamacarolina@gmail.com>
2022-07-01 20:14:39 -0300

    add name and new sentence in program.cs

--- RecuperatoriosTP/TP_2/TP-02/Program.cs
+++ RecuperatoriosTP/TP_2/TP-02/Program.cs
@@ -16,7 +16,7 @@ namespace TP_02_2018 // Pusheado
             Console.SetWindowSize(Console.LargestWindowWidth / 2, Console.LargestWindowHeight - 2);
 
             // Nombre del alumno
-            Console.Title = "";
+            Console.Title = "Carolina Yokoyama";
 
             Taller taller = new Taller(6);
 
@@ -45,9 +45,16 @@ namespace TP_02_2018 // Pusheado
             Console.ReadKey();
             Console.Clear();
 
+            /* 
             // Quito 2 items y muestro
             taller -= c1;
             taller -= new Ciclomotor(Vehiculo.EMarca.Honda, "ASD913", ConsoleColor.Red);
+            */
+
+            // Quito 2 items y muestro
+            taller -= c1;
+            taller -= new Ciclomotor(Vehiculo.EMarca.HarleyDavidson, "LEM666", ConsoleColor.Red);
+
 
             Console.WriteLine(taller.ToString());
             Console.WriteLine("<-----------PRESIONE UNA TECLA PARA CONTINUAR----------->");
